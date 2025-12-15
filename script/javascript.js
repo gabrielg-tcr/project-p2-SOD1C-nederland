96dfa9efdf5aadfb4b072ca99376c5533d58bf39
JackLee9028664 <9028664@student.zadkine.nl>
2025-12-15 13:27:14 +0100

    religion/style.css en javscript veranderd

--- script/javascript.js
+++ script/javascript.js
@@ -1,5 +1,31 @@
 console.log("hallo");
 
-function test() {
-document.getElementById("religion").innerHTML = ("test");
+function islam() {
+document.getElementById("description").innerHTML = ("in nederland is 6% procent moslim. De islam is een monotheïstische" + 
+    "religie die gebaseerd is op de overgave aan de wil van Allah. Mohammed is de profeet en de Koran is het heilige boek. Moslims bidden vijf keer per dag, vasten tijdens de ramadan, helpen anderen en volgen leefregels zoals geen alcohol en geen varkensvlees. Ze geloven ook in leven na de dood");
+document.body.style.backgroundColor = "";
+}
+
+function katholicisme() {
+document.getElementById("description").innerHTML = ("Katholicisme is een christelijke geloofstraditie met de paus als hoofd, waarin liturgie, sacramenten en de verering van Maria en heiligen centraal staan. Het woord katholiek komt uit het Grieks en betekent algemeen of universeel.heeft een duidelijke, centraal geleide structuur onder leiding van de paus.");
+
+}
+
+function joden() {
+document.getElementById("description").innerHTML = ("Het Jodendom is de godsdienst en cultuur die bij het Joodse volk hoort. Het is een van de oudste godsdiensten waarbij mensen in één God geloven en bestaat zo’n 3.500 jaar. De Tora is de belangrijkste basis van het Jodendom. Deze heilige tekst bestaat uit 5 boeken kent verschillende stromingen met uiteenlopende regels en interpretaties, en heeft geen centraal religieus gezag.");
+
+}
+
+function protestanteisme() {
+document.getElementById("description").innerHTML = (" protestantse godsdienst is gebaseerd op de leerstellingen van de Reformatie, met nadruk op de genade van God, het priesterschap van alle gelovigen en de rechtvaardiging door geloof alleen. Protestanten hechten waarde aan de verkondiging van het Woord en de viering van de sacramenten.");
+
+}
+
+function hindoeisme() {
+document.getElementById("description").innerHTML = ("Qua praktijken en filosofieën omvat het hindoeïsme een breed scala van wetten en voorschriften van dagelijkse moraal, gebaseerd op de concepten van karma en dharma en op maatschappelijke normen, zoals hindoeïstische huwelijksgebruiken");
+}
+
+function Boeddhisme() {
+document.getElementById("description").innerHTML = ("Het boeddhisme is een religie. Het doel van boeddhisten is om te ontsnappen uit de cirkel van wedergeboorten, oftewel de samsara. Verlossing van het lijden speelt een centrale rol binnen het boeddhisme. Je kunt alleen van het lijden verlost worden door het edele achtvoudige pad te bewandelen.");
+
 }
