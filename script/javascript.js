5609c1b133d67f9a7feb27b3b66bfe1168d2cbf3
JackLee9028664 <9028664@student.zadkine.nl>
2026-01-26 12:32:25 +0100

    bewerkingen
    doelstelling aangepast en af.
    formulier donatie af, goede doelen af religion pagina bewerkt tabel bevolking af

--- script/javascript.js
+++ script/javascript.js
@@ -1,5 +1,32 @@
 console.log("hallo");
 
+document.getElementById("tabel_header").style.backgroundColor  = "blue";
+
+function toonfoto() {
+document.getElementById("thankyou").style.display = "block";
+}
+
+document.getElementById("thankyou").style.display = "none";
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
 function islam() {
 document.getElementById("description").innerHTML = ("in nederland is 6% procent moslim. De islam is een monotheïstische" + 
     "religie die gebaseerd is op de overgave aan de wil van Allah. Mohammed is de profeet en de Koran is het heilige boek. Moslims bidden vijf keer per dag, vasten tijdens de ramadan, helpen anderen en volgen leefregels zoals geen alcohol en geen varkensvlees. Ze geloven ook in leven na de dood");
@@ -7,18 +34,6 @@ document.body.style.backgroundColor = "";
 }
 
 function katholicisme() {
-document.getElementById("description").innerHTML = ("Katholicisme is een christelijke geloofstraditie met de paus als hoofd, waarin liturgie, sacramenten en de verering van Maria en heiligen centraal staan. Het woord katholiek komt uit het Grieks en betekent algemeen of universeel.heeft een duidelijke, centraal geleide structuur onder leiding van de paus.");
-
-}
-
-function joden() {
-document.getElementById("description").innerHTML = ("Het Jodendom is de godsdienst en cultuur die bij het Joodse volk hoort. Het is een van de oudste godsdiensten waarbij mensen in één God geloven en bestaat zo’n 3.500 jaar. De Tora is de belangrijkste basis van het Jodendom. Deze heilige tekst bestaat uit 5 boeken kent verschillende stromingen met uiteenlopende regels en interpretaties, en heeft geen centraal religieus gezag.");
-
-}
-
-function protestanteisme() {
-document.getElementById("description").innerHTML = (" protestantse godsdienst is gebaseerd op de leerstellingen van de Reformatie, met nadruk op de genade van God, het priesterschap van alle gelovigen en de rechtvaardiging door geloof alleen. Protestanten hechten waarde aan de verkondiging van het Woord en de viering van de sacramenten.");
-
 }
 
 function hindoeisme() {
