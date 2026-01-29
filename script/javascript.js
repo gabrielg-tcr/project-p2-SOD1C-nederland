99b2d8978436222d773a7929bb7a1d54b3e8f507
JackLee9028664 <9028664@student.zadkine.nl>
2026-01-29 11:36:55 +0100

    countdown timer, en bron vermelding gemaakt

--- script/javascript.js
+++ script/javascript.js
@@ -1,31 +1,11 @@
 console.log("hallo");
 
-document.getElementById("tabel_header").style.backgroundColor  = "blue";
 
 function toonfoto() {
 document.getElementById("thankyou").style.display = "block";
 }
 
-document.getElementById("thankyou").style.display = "none";
 
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    }
-  }
-}
 
 function islam() {
 document.getElementById("description").innerHTML = ("in nederland is 6% procent moslim. De islam is een monotheïstische" + 
@@ -44,3 +24,43 @@ function Boeddhisme() {
 document.getElementById("description").innerHTML = ("Het boeddhisme is een religie. Het doel van boeddhisten is om te ontsnappen uit de cirkel van wedergeboorten, oftewel de samsara. Verlossing van het lijden speelt een centrale rol binnen het boeddhisme. Je kunt alleen van het lijden verlost worden door het edele achtvoudige pad te bewandelen.");
 
 }
+
+function dark() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("moon").innerHTML = "⋆⁺₊⋆ ☾⋆⁺₊⋆";
+
+}
+
+function light() {
+  document.body.style.backgroundColor = "white";
+    document.getElementById("sun").innerHTML = "⋆｡ ﾟ☁︎｡ ⋆｡ ﾟ☀︎｡ ⋆｡ ﾟ";
+}
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (90000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (20000 * 60 * 60 * 24)) / (90000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "the periode is bijna afgelopen";
+  }
+}, 1000);
